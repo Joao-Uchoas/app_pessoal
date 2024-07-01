30431fd2fa987150c8583520df229268a1e2ecc0
Joao-Uchoas <70238880+Joao-Uchoas@users.noreply.github.com>
2024-07-01 10:10:01 -0300

    Initial Commit

---
+++ app_pessoal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app_pessoal
+//
+//  Created by Uchoas Santos, Joao Vitor on 01/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app_pessoalUITests/app_pessoalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  app_pessoalUITestsLaunchTests.swift
+//  app_pessoalUITests
+//
+//  Created by Uchoas Santos, Joao Vitor on 01/07/24.
+//
+
+import XCTest
+
+final class app_pessoalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
